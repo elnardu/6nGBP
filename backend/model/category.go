90afb9650c8b68b569c366d28e4a4e3e874a0218
elnardu <elnardu2@gmail.com>
2017-12-09 16:29:09 -0500

    Improved error checking

--- backend/model/category.go
+++ backend/model/category.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"errors"
 	"log"
 
 	"github.com/elnardu/6nGBP/backend/types"
@@ -15,18 +16,30 @@ func (db *MyDB) GetCategoryByID(id *bson.ObjectId) *types.Category {
 }
 func (db *MyDB) GetCategories() *[]*types.Category {
 	result := &[]*types.Category{}
-	db.C("categories").Find(bson.M{}).Sort("title").Limit(20).All(result)
+	db.C("categories").Find(bson.M{}).Sort("title").All(result)
 	return result
 }
 func (db *MyDB) CreateCategory(category *types.Category) error {
+	if category.Title == "" {
+		return errors.New("Title cannot be empty")
+	}
+
+	if category.Description == "" {
+		return errors.New("Description cannot be empty")
+	}
+
 	category.ID = bson.NewObjectId()
 	err := db.C("categories").Insert(category)
+	if err != nil && err.Error()[:6] == "E11000" {
+		return errors.New("You cannot use this title")
+	}
 	return err
 }
 
 func (db *MyDB) InitCategories() {
 	titleIndex := mgo.Index{
-		Key: []string{"title"},
+		Key:    []string{"title"},
+		Unique: true,
 	}
 	err := db.C("categories").EnsureIndex(titleIndex)
 	if err != nil {
